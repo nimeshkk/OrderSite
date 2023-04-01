bfba6645a79021c314cdc1402c29e99b9cee2a2e
nimeshkk <nimeshkliyanage@gmail.com>
2023-04-01 20:28:56 +0530

    c2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let navbar =document.querySelector('.header .navbar ');
+let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick  = () =>{
+document.querySelector('#menu-btn').onclick = () =>{
+    
     navbar.classList.add('active');
 }
 
-document.querySelector('#nav-close').onclick =() =>
-{
-    navbar.classList.remove('active');
-}
-window.onscroll = () =>
-{
-    navbar.classList.remove('active');
-}
+// document.querySelector('#nav-close').onclick = () =>
+// {
+//     n.classList.remove('active');
+// }
+// window.onscroll = () =>
+// {
+//     n.classList.remove('active');
+// }
+
+
