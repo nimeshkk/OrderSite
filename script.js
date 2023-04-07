1bff2003b3a7711bf59c4ca9acb2f2ca35078ae2
nimeshkk <nimeshkliyanage@gmail.com>
2023-04-07 15:07:46 +0530

    dropdown

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+ 
+//dropdown
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
